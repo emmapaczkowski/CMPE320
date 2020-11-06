2509e1b4d76755800319c6bd09f96aa820878fcc
Emma <17ep14@queensu.ca>
2020-11-06 08:14:42 -0500

    Add Qt Project to workspace.

---
+++ Assignment3/Fraction/fraction.cpp
@@ -0,0 +1,5 @@
+//#include "Error in " Util.relativeFilePath('C:/Users/emmap/OneDrive - Queen's University/Third Year/CMPE320/Assignment3/fraction.h', 'C:/Users/emmap/OneDrive - Queen's University/Third Year/CMPE320/Assignment3' + '/' + Util.path('fraction.cpp'))": SyntaxError: Expected token `)'"
+#include "fraction.h"
+Fraction::Fraction()
+{
+}
---
+++ Assignment3/Fraction/fraction.h
@@ -0,0 +1,16 @@
+#ifndef FRACTION_H
+#define FRACTION_H
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+#include "Fraction_global.h"
+
+class FRACTION_EXPORT Fraction
+{
+public:
+    Fraction();
+};
+
+#endif // FRACTION_H
