bd8425633bf870d6721751522cc72f12cfa778a1
Emma <17ep14@queensu.ca>
2020-11-20 10:09:31 -0500

    Add header file with attributes.

---
+++ Assignment4/src/jumble.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+
+class JumblePuzzle {
+
+    private:
+
+    public:
+
+    JumblePuzzle(string toHide, string difficulty);
+
+    charArrayPtr* getJumble();  //NOT SURE WHAT THE RETURN TYPW SHOULD BE
+    string getSize();
+
+
+};
